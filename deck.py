42b8de471fd524befd32d071348434877f9588bb
Rafael Barbosa <rafaellb2555@gmail.com>
2022-11-24 21:51:00 -0300

    CHORE: Change deck approach to use Classes.

--- deck.py
+++ deck.py
@@ -1,29 +1,34 @@
 import requests
 
-deck = requests.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+class Deck:
+    def __init__(self):
+        deck = requests.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+        self.deck = deck.json()
+        self.deck_id = deck['deck_id']
 
-response = deck.json()
+    
+    def draw_card(self) -> dict:
+        requests.get(f"https://deckofcardsapi.com/api/deck/{self.deck_id}/shuffle/")
+        response = requests.get(f"https://deckofcardsapi.com/api/deck/{self.deck_id}/draw/?count=1")
+        card = response.json()
+        return card
+        
 
-twentyonedeck = response["deck_id"]
+    def new_game(self) -> list:
+        card1 = self.draw_card()
+        card2 = self.draw_card()
 
+        return [card1, card2]
 
-def new_game(deck_id):
-    shuffle = requests.get(f"https://deckofcardsapi.com/api/deck/{deck_id}/shuffle/")
-    draw2card = requests.get(
-        f"https://deckofcardsapi.com/api/deck/{deck_id}/draw/?count=2"
-    )
-    card = draw2card.json()
-    return [card["cards"][0]["value"], card["cards"][1]["value"]]
-
-
-def draw_one_card(deck_id):
-    draw1card = requests.get(
-        f"https://deckofcardsapi.com/api/deck/{deck_id}/draw/?count=1"
-    )
-    card = draw1card.json()
-    return card["cards"][0]["value"]
-
-
-cards = new_game(twentyonedeck)
-
-new_card = draw_one_card(twentyonedeck)
+    
+# 1- Criar novo deck. 
+# 2- Pega duas cartas para o player:
+#   2.1 - Shuffle no deck
+#   2.2 - Pegar duas cartas
+# 3- Somar o valor das cartas
+# 4- Perguntar para o player se ele deseja uma nova carta
+# 5- Caso deseje, puxar mais uma carta, adicionar às cartas do player 
+# 5.1- Caso o valor esteja abaixo de 21, perguntar se ainda quer carta
+# 5.2- Caso seja 21, ele ganhou
+# 5.3- Caso passe de 21, ele perdeu
+# 5.4- Caso ele não deseje mais cartas, pegue cartas para o crupiê
